d9a73dd7a8cb22520d5a3549dca2ef70da23cfef
Denver7074 <119703412+Denver7074@users.noreply.github.com>
2022-12-22 15:11:38 +0300

    hw6
    hw6

---
+++ HW4-5/src/main/java/Main.java
@@ -0,0 +1,14 @@
+import java.lang.reflect.Array;
+import java.math.BigInteger;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String[] args) {
+        HoweWork howeWork = new HoweWork();
+        System.out.println(howeWork.mass(new Integer[]{1, 2, 3, 2, 0},new Integer[]{5, 1, 2, 0, 3, 2}));
+    }
+
+}
